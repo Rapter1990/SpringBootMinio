bbcea636acbc3db072f225e3b2d61a3d033ffd60
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-01-13 12:30:08 +0300

    Task 1 : Initial commit

---
+++ src/test/java/com/springminio/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springminio.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
