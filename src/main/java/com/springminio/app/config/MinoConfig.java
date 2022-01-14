629aab23e503018388876be0e40813916835c675
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2022-01-14 12:06:23 +0300

    Task 4 : Define MinoConfig and Change application.properties to application.yml

---
+++ src/main/java/com/springminio/app/config/MinoConfig.java
@@ -0,0 +1,47 @@
+package com.springminio.app.config;
+
+import io.minio.MinioClient;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "minio")
+public class MinoConfig {
+
+    /** *  It's a URL, domain name ,IPv4 perhaps IPv6 Address ") */
+    private String endpoint;
+
+    /** * //"TCP/IP Port number " */
+    private Integer port;
+
+    /** * //"accessKey Similar to user ID, Used to uniquely identify your account " */
+    private String accessKey;
+
+    /** * //"secretKey It's the password for your account " */
+    private String secretKey;
+
+    /** * //" If it is true, It uses https instead of http, The default value is true" */
+    private boolean secure;
+
+    /** * //" Default bucket " */
+    private String bucketName;
+
+    /** *  The maximum size of the picture  */
+    private long imageSize;
+
+    /** *  Maximum size of other files  */
+    private long fileSize;
+
+    @Bean
+    public MinioClient minioClient() {
+        MinioClient minioClient =
+                MinioClient.builder()
+                        .credentials(accessKey, secretKey)
+                        .endpoint(endpoint,port,secure)
+                        .build();
+        return minioClient;
+    }
+}
